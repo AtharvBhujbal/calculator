1628be9363badd5c47bacc524f6c4a1fc0bd07fd
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-05-13 01:20:28 +0530

    Updated script.js with eventlistener function

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1/num2;
 };
+let button=document.querySelectorAll('button');
+button.forEach(button=>button.addEventListener('click',getclass));
+function getclass(e){
+    if(isNaN(button.classList)) {
+        operator=button.classList;
+    }
+    else{
+        num1=""+button.classList ;
+    }
+}
+function operate(num1,num2,operator){
+    return operator(num1,num2);
+}
